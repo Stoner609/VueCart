0fe8abb689ecab8e5774b9d4856e97be6b1aa452
Stoner609 <stoner7969@gmail.com>
2018-04-17 00:21:59 +0800

    update

---
+++ src/LessonList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="list">
+      <LessionItem
+        v-for="lesson in lessons"
+        :lesson=lesson
+        :key=lesson.id
+      />
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+import LessionItem from './LessonItem.vue'
+
+export default {
+//   computed: {
+//       lessons() {
+//            return this.$store.state.lessons;
+//       },
+//   },
+//   mounted() {
+//     this.$store.dispatch('fetchLessons');
+//   },
+    components: {
+        LessionItem,
+    },
+    computed: {
+        ...mapState(['lessons']),
+    },
+    methods: {
+        ...mapActions(['fetchLessons']),
+    },
+    mounted() {
+        this.fetchLessons();
+    },
+}
+</script>
+
+<style scoped>
+.list{
+  padding-top: 30px;
+  text-align: center;
+}
+</style>

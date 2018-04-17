104be0e6701c39fa2a58e9bdad5199565b4e9874
Stoner609 <stoner7969@gmail.com>
2018-04-18 00:17:54 +0800

    搜尋功能

--- src/LessonList.vue
+++ src/LessonList.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="list">
+      <div class="divStyle">
+        <input type="text" class="inputStyle" v-model="text" />
+      </div>
       <LessionItem
-        v-for="(lesson, idx) in lessons"
+        v-for="(lesson, idx) in searchLessons"
         :lesson=lesson
         :key=lesson.id
       />
@@ -27,6 +30,18 @@ export default {
     },
     computed: {
         ...mapState(['lessons']),
+        searchLessons() {
+            return this.lessons.filter(x => {
+                if (x.title.indexOf(this.text) > -1) {
+                    return x
+                }
+            });
+        }
+    },
+    data() {
+        return {
+            text: '',
+        }
     },
     methods: {
         ...mapActions(['fetchLessons']),
@@ -41,5 +56,17 @@ export default {
 .list{
   padding-top: 30px;
   text-align: center;
+}
+.divStyle {
+    padding: 15px;
+    text-align: center;
+    
+}
+input[type=text] {
+    width: 300px;
+    border: 1px solid transparent;
+    background-color: #f1f1f1;
+    padding: 10px;
+    font-size: 16px;
 }
 </style>
